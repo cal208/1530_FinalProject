c77c77eec57a75f90b031b2a484cb9fe7e88da17
lsb40 <92069369+lsb40@users.noreply.github.com>
2024-04-21 12:27:47 -0400

    Update MoneyMinder.java

--- MoneyMinder.java
+++ MoneyMinder.java
@@ -5,6 +5,7 @@ public class MoneyMinder {
         String menuString = "------------------------ \n     MONEY MINDER \n------------------------ \n1. Add Expense \n2. Add income \n3. Exit \nPlease choose an option: ";
         Scanner scanner = new Scanner(System.in);
         int choice;
+        User user = new User();
         while(true) {
             System.out.print(menuString);
             choice = scanner.nextInt();
@@ -13,13 +14,32 @@ public class MoneyMinder {
                     System.out.println("ADD EXPENSE METHOD HERE");
                     break;
                 case 2:
-                    System.out.println("ADD INCOME METHOD HERE");
+                    addIncome(user, scanner);
                     break;
                 case 3:
                     System.exit(0);
                 default:
                     System.out.println("NOT A VALID CHOICE");
             }
+        }
+    }
+    public static void addIncome(User user, Scanner scanner) {
+        while (true) {
+            System.out.print("Enter income amount (enter 0 to stop): ");
+            double amount = scanner.nextDouble();
+            if (amount == 0) {
+                break;
+            }
+            scanner.nextLine(); 
+            System.out.print("Enter income source: ");
+            String source = scanner.nextLine();
+
+            user.addIncome(amount, source);
+        }
+
+        System.out.println("User's incomes:");
+        for (Map<String, Object> income : user.getIncomes()) {
+            System.out.println("Amount: $" + income.get("amount") + ", Source: " + income.get("source"));
         }
     }
 }
